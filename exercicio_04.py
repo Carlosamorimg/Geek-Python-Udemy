a1303da230153ab7add8ae3349e0b8294a6a16e8
Carlosamorimg <102837569+Carlosamorimg@users.noreply.github.com>
2023-09-06 15:21:49 -0300

    3º

---
+++ exercicio_04.py
@@ -0,0 +1,7 @@
+num = float(input("Digite um numero decimal "))
+quadrado = num ** 2
+print(f'O quadrado de {num} é {quadrado}')
+
+# 2º forma
+number = float(input('Informe um número real: '))
+print('{} elevado ao quadrado = {:.2f}'.format(number, number**2))
